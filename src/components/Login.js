1ad40061331f97f223d2b3ec1c7b88498cd6d23e
Glitch-cypher <cahc22@gmail.com>
2021-03-16 13:41:41 +0000

    applied changes to login component to match signup component

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,30 +4,40 @@ import { AccountContext } from "./Accounts";
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [err,setErr] = useState('');
+  const [checked,setChecked] = useState(true);
   const { authenticate } = useContext(AccountContext);
 
   const onSubmit = (event) => {
     event.preventDefault();
+    setErr('');
     authenticate(email, password)
       .then((data) => {
         console.log("logged in!", data);
       })
       .catch((err) => {
-        console.error("Failed to login!", err);
+        setErr( err.message);
       });
   };
+
+  function passwordToggle() {
+    setChecked(!checked);
+}
 
   return (
     <div>
       <form onSubmit={onSubmit}>
+      <p style = {{fontSize: '1em', color: 'red' }}>{err}</p>
         <input
           value={email}
           onChange={(event) => setEmail(event.target.value)}
         />
         <input
+        type = {checked? 'password':'text'}
           value={password}
           onChange={(event) => setPassword(event.target.value)}
         />
+        <input type = 'checkbox' checked = {checked} onClick = {()=>passwordToggle()}/>
         <button type="submit">Login</button>
       </form>
     </div>
