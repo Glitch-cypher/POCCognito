730de68e66dba9f54f3d75e87aceded9c524ced4
cpanda3 <ameliacollinspatel@gmail.com>
2021-03-17 09:51:16 +0000

    comments on user pool

--- src/UserPool.js
+++ src/UserPool.js
@@ -1,5 +1,7 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js";
 
+//PoolData provides the information from the AWS cognito Pool. 
+
 const poolData = {
   UserPoolId: "eu-west-1_a6Z6hPgM0",
   ClientId: "7osp2h8fs50p8cf3e2hhflqos5",
