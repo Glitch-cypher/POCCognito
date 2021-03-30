cfc61d147f7747d13f0d19e2675c7ed4e9a78135
Glitch-cypher <cahc22@gmail.com>
2021-03-30 15:59:18 +0100

    refactored App.js to make it more easily readable

---
+++ src/components/CookieBanner.js
@@ -0,0 +1,51 @@
+export default function CookieBanner({ serviceName }) {
+  return (
+    <div
+      className="govuk-cookie-banner "
+      role="region"
+      aria-label="Cookies on [name of service]"
+    >
+      <div className="govuk-cookie-banner__message govuk-width-container">
+        <div className="govuk-grid-row">
+          <div className="govuk-grid-column-two-thirds">
+            <h2 className="govuk-cookie-banner__heading govuk-heading-m">
+              Cookies on {serviceName}
+            </h2>
+
+            <div className="govuk-cookie-banner__content">
+              <p>We use some essential cookies to make this service work.</p>
+              <p>
+                We'd also like to use analytics cookies so we can understand how
+                you use the service and make improvements.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="govuk-button-group">
+          <button
+            value="accept"
+            type="button"
+            name="cookies"
+            className="govuk-button"
+            data-module="govuk-button"
+          >
+            Accept analytics cookies
+          </button>
+          <button
+            value="reject"
+            type="button"
+            name="cookies"
+            className="govuk-button"
+            data-module="govuk-button"
+          >
+            Reject analytics cookies
+          </button>
+          <a className="govuk-link" href="/">
+            View cookies
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
